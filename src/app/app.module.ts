dbf9521c69fa2ac9e7b332f9d6275bf0ce12e624
Nimit Bhargava <nimitbhargava@gmail.com>
2017-09-23 12:45:39 +0530

    Instead of importing MaterialModule, we import the individual Material Component Modules, else we will get error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from '@angular/material'; 
+import { MdToolbarModule, MdButtonModule, MdCheckboxModule } from '@angular/material'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
@@ -15,8 +15,10 @@ import 'hammerjs';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MdToolbarModule, 
+    MdButtonModule, 
+    MdCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
